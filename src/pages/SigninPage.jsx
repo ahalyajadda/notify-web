931e0648b0d830d8dfbe0a0c8f59fb7621d1b69b
ahalyajadda <varshithajadda@gmail.com>
2022-11-13 17:43:43 +0000

    first commit

---
+++ src/pages/SigninPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Signin from "../components/Signin/Signin";
+import "./Signinpage.css";
+function SigninPage() {
+    return (
+        <div className="sign">
+            <Signin />
+        </div>
+    );
+}
+
+export default SigninPage;
